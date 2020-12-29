6c855b7b7395678256e09e462659f63d92ca2dc6
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2020-12-29 22:40:58 +0000

    UI done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,10 @@
         <h1>Rants and Confessions</h1>
 
         <div class="message-area">
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <form action="">
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <input type="submit" value="Send message">
+            </form>
         </div>
     </section>
 
