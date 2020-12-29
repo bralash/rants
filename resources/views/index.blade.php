a6207eeec3bef6605fce6b4ef985649f07bbc275
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2020-12-29 22:28:00 +0000

    Textarea

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,11 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anonymous Messages | Rants and Confessions</title>
     <link rel="stylesheet" href="{{URL::asset('css/style.css')}}" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
 </head>
 <body>
     
     <section>
         <h1>Rants and Confessions</h1>
+
+        <div class="message-area">
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+        </div>
     </section>
 
 </body>
