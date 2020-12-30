af0d614a363aad71854f6841d9e542b94b358200
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2020-12-30 00:41:27 +0000

    Database approach

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use App\Models\Message;
 
 class MessageController extends Controller
 {
@@ -12,16 +13,21 @@ class MessageController extends Controller
     }
 
     public function message(Request $request) {
-        $rant = $request->message;
+        
+        // $to_name = 'Rants and Confesstions';
+        // $to_email = 'rantsnconfess@gmail.com';
+        // $data = array("name" => "Rev. Father", "body" => $rant);
 
-        $to_name = 'Rants and Confesstions';
-        $to_email = 'rantsnconfess@gmail.com';
-        $data = array("name" => "Rev. Father", "body" => $rant);
+        // Mail::send("emails.mail", $data, function($message) use ($to_name, $to_email) {
+        //     $message->to($to_email, $to_name)->subject('Confession Entry');
+        //     $message->from('no-reply@gmail.com','Anonymouse Message');
+        // });
 
-        Mail::send("emails.mail", $data, function($message) use ($to_name, $to_email) {
-            $message->to($to_email, $to_name)->subject('Confession Entry');
-            $message->from('no-reply@gmail.com','Anonymouse Message');
-        });
+        $rant = new Message;
+        $rant->message = $request->message;
+        
+
+        $rant->save();
 
         return View('thanks');
     }
