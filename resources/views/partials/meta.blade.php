a90dc7b62bfac73448c9412e8cccd0ca442e8ce0
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2021-03-23 08:55:27 +0000

    Final URL fix

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -1,10 +1,10 @@
 <meta property="og:type" content="website">
-<meta property="og:title" content="Anonymous Confessions">
-<meta property="og:description" content="Allows listeners to send in anonymous messages for our podcast">
-<meta property="og:url" content="http://rantsnconfess.com/">
+<meta property="og:title" content="Rants and Confessions">
+<meta property="og:description" content="Official anonymous messaging platform for Rants and Confessions Podcast">
+<meta property="og:url" content="https://rantsnconfess.com/">
 <meta property="og:site_name" content="Rants and Confessions">
-<meta property="og:image" content="http://rantsnconfess.com/img/favicon.jpg">
-<meta property="og:image:secure_url" content="http://rantsnconfess.com/img/favicon.jpg">
+<meta property="og:image" content="https://rantsnconfess.com/img/favicon.jpg">
+<meta property="og:image:secure_url" content="https://rantsnconfess.com/img/favicon.jpg">
 <meta property="og:image:width" content="1000">
 <meta property="og:image:height" content="1000">
 <meta property="og:image:alt" content="anonymous confessions">
