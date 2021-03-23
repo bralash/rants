535244cd78516199ebaa97a935f4c967be912328
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2021-03-23 09:51:35 +0000

    Meta reverted

--- resources/views/audio.blade.php
+++ resources/views/audio.blade.php
@@ -10,7 +10,16 @@
     <script src="https://use.fontawesome.com/4b9b01d581.js"></script>
 
 
-    @include('partials.meta')
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Rants and Confessions">
+    <meta property="og:description" content="Official anonymous messaging platform for Rants and Confessions Podcast">
+    <meta property="og:url" content="https://rantsnconfess.com/">
+    <meta property="og:site_name" content="Rants and Confessions">
+    <meta property="og:image" content="https://rantsnconfess.com/img/favicon.jpg">
+    <meta property="og:image:secure_url" content="https://rantsnconfess.com/img/favicon.jpg">
+    <meta property="og:image:width" content="1000">
+    <meta property="og:image:height" content="1000">
+    <meta property="og:image:alt" content="anonymous confessions">
 </head>
 <body>
     
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,16 @@
     <link rel="icon" href="{{URL::asset('img/favicon.jpg')}}" type="image/gif" sizes="16x16"> 
 
     
-    @include('partials.meta')
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Rants and Confessions">
+    <meta property="og:description" content="Official anonymous messaging platform for Rants and Confessions Podcast">
+    <meta property="og:url" content="https://rantsnconfess.com/">
+    <meta property="og:site_name" content="Rants and Confessions">
+    <meta property="og:image" content="https://rantsnconfess.com/img/favicon.jpg">
+    <meta property="og:image:secure_url" content="https://rantsnconfess.com/img/favicon.jpg">
+    <meta property="og:image:width" content="1000">
+    <meta property="og:image:height" content="1000">
+    <meta property="og:image:alt" content="anonymous confessions">
 </head>
 <body>
     
