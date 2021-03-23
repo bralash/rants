22fea20c905049caa3dd2d56207e40d06097536b
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2021-03-23 08:37:59 +0000

    Fixed url error

--- resources/views/audio.blade.php
+++ resources/views/audio.blade.php
@@ -10,16 +10,7 @@
     <script src="https://use.fontawesome.com/4b9b01d581.js"></script>
 
 
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="Anonymous Confessions">
-    <meta property="og:description" content="Allows listeners to send in anonymous messages for our podcast">
-    <meta property="og:url" content="http://rantsnconfess.com/">
-    <meta property="og:site_name" content="Rants and Confessions">
-    <meta property="og:image" content="http://rantsnconfess/img/favicon.jpg">
-    <meta property="og:image:secure_url" content="http://rantsnconfess/img/favicon.jpg">
-    <meta property="og:image:width" content="1000">
-    <meta property="og:image:height" content="1000">
-    <meta property="og:image:alt" content="anonymous confessions">
+    @include('partials.meta')
 </head>
 <body>
     
