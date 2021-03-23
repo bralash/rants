254e2ecf40c2f0db0a48e9533eefb974595dec5b
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2021-03-23 08:32:25 +0000

    Meta tags updated

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anonymous Messages | Rants and Confessions</title>
     <link rel="stylesheet" href="{{URL::asset('css/style.css')}}" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
     <link rel="icon" href="{{URL::asset('img/favicon.jpg')}}" type="image/gif" sizes="16x16"> 
+
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Anonymous Confessions">
+    <meta property="og:description" content="Allows listeners to send in anonymous messages for our podcast">
+    <meta property="og:url" content="http://rantsnconfess.com/">
+    <meta property="og:site_name" content="Rants and Confessions">
+    <meta property="og:image" content="http://rantsnconfess/img/favicon.jpg">
+    <meta property="og:image:secure_url" content="http://rantsnconfess/img/favicon.jpg">
+    <meta property="og:image:width" content="1000">
+    <meta property="og:image:height" content="1000">
+    <meta property="og:image:alt" content="anonymous confessions">
 </head>
 <body>
     
