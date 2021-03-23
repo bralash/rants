60d67f8872d008d5ea0788d9ef204b46147b4753
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2021-03-23 10:02:59 +0000

    Meta title

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -9,16 +9,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
     <link rel="icon" href="{{URL::asset('img/favicon.jpg')}}" type="image/gif" sizes="16x16"> 
 
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="Rants and Confessions">
-    <meta property="og:description" content="Official anonymous messaging platform for Rants and Confessions Podcast">
-    <meta property="og:url" content="https://rantsnconfess.com/">
-    <meta property="og:site_name" content="Rants and Confessions">
-    <meta property="og:image" content="https://rantsnconfess.com/img/favicon.jpg">
-    <meta property="og:image:secure_url" content="https://rantsnconfess.com/img/favicon.jpg">
-    <meta property="og:image:width" content="1000">
-    <meta property="og:image:height" content="1000">
-    <meta property="og:image:alt" content="anonymous confessions">
+    @include('partials.meta')
 </head>
 <body>
     
