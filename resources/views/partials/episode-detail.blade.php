e624ae7c3c22a186dd719ebe30fb7e1f48f50324
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2021-04-04 21:32:44 +0000

    Minor changes to episode meta

--- resources/views/partials/episode-detail.blade.php
+++ resources/views/partials/episode-detail.blade.php
@@ -1,16 +1,17 @@
-<meta property="og:type" content="website">
-<meta property="og:title" content="{{$episode->title}} by Rants and Confessions">
-<meta property="og:description" content="{{$episode->description}}">
-<meta property="og:url" content="https://rantsnconfess.com/episodes">
-<meta property="og:site_name" content="{{$episode->title}} by Rants and Confessions">
-<meta property="og:image" content="https://rantsnconfess.com/img/favicon.jpg">
-<meta property="og:image:secure_url" content="https://rantsnconfess.com/img/favicon.jpg">
-<meta property="og:image:width" content="1000">
-<meta property="og:image:height" content="1000">
-<meta property="og:image:alt" content="{{$episode->title}}">
-<meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:site" content="@rantsnconfess">
-<meta name="twitter:creator" content="@rantsnconfess">
-<meta name="twitter:title" content="{{$episode->title}} by Rants and Confessions">
-<meta name="twitter:description" content="{{$episode->description}}">
-<meta name="twitter:image" content="https://rantsnconfess.com/img/favicon.jpg">
+
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="{{$episode->title}} by Rants and Confessions">
+    <meta property="og:description" content="{{$episode->description}}">
+    <meta property="og:url" content="https://rantsnconfess.com/episode/{{$episode->slug}}">
+    <meta property="og:site_name" content="{{$episode->title}} by Rants and Confessions">
+    <meta property="og:image" content="{{$episode->img_url}}">
+    <meta property="og:image:secure_url" content="{{$episode->img_url}}">
+    <meta property="og:image:width" content="1000">
+    <meta property="og:image:height" content="1000">
+    <meta property="og:image:alt" content="{{$episode->title}}">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@rantsnconfess">
+    <meta name="twitter:creator" content="@rantsnconfess">
+    <meta name="twitter:title" content="{{$episode->title}} by Rants and Confessions">
+    <meta name="twitter:description" content="{{$episode->description}}">
+    <meta name="twitter:image" content="{{$episode->img_url}}">
